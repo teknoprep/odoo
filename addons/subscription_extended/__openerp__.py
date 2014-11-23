637989f7b2eacab6477ce4e462b6f2c16f7b155e
teknoprep <cm.rawlings@gmail.com>
2014-11-23 15:19:28 -0500

    [FIX] Subscription_Extended Module
    had a revert before this and kept the nomenclature the same as everyone
    elses modules
    updated it to fix it so it actually creates invoices
    updated it to allow taxing of items
    updated so that it shows up inside accounting module
    few other bug fixes

--- addons/subscription_extended/__openerp__.py
+++ addons/subscription_extended/__openerp__.py
@@ -1,44 +1,27 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
 
 {
-    'name': 'Subscription Extented',
-    'version': '1.0',
+    'name': 'Recurring Documents Plus',
+    'version': '1.5',
     'category': 'Customization',
     'summary': '',
     'description': """
+            Subscription Extended Module for Odoo V8
 
     """,
-    'author': 'Pragtech',
-    'website': 'http://www.pragtech.co.in',
+    'author' : 'ZedeS Technologies',
+    'website' : 'http://www.zedestech.com',
     'depends': ['subscription', 'sale'],
     'data': [],
     'demo': [],
     'test': [],
     'update_xml': [
-                   'security/ir.model.access.csv',
-                   'data_sequence.xml',
-                   'subscription_custom_view.xml',
+                    'subscription_view.xml',
+                    'sale_template_view.xml',
+
+                    'security/ir.model.access.csv',
+                    
         ],
     'installable': True,
     'auto_install': False,
 }
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
